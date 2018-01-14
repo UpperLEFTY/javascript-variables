d32ebaff64e9f347f5f05fa814083a2a1231afa2
Essence Alexander <upperlefty@zoho.eu>
2018-01-14 15:42:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-for(var i = 0;  i<10; i++)
+/**
+* The first fucntion prints out numbers 0-9 then the last console log will print the value 10.
+This is because i is still valid outside the for loop — the scope of i is not bounded by the curly braces {} 
+  */
+
+for(var i = 0;  i<10; i++)  {
+  console.log(i)
+}
+  console.log(i)
+
