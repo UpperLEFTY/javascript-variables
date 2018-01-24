28ac27fada5b30e314d0331a655a66ea84bece64
Essence Alexander <upperlefty@zoho.eu>
2018-01-24 21:13:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 This is because i is still valid outside the for loop — the scope of i is not bounded by the curly braces {} 
   */
 
-for(var i = 0;  i<10; i++)  {
+(var i = 0;  i<10; i++)  {
   console.log(i)
 }
   console.log(i)
