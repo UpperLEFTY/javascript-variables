15dfb97b85fd8fb48b4986363521d00269807262
Essence Alexander <upperlefty@zoho.eu>
2018-03-02 10:03:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
-* The first fucntion prints out numbers 0-9 then the last console log will print the value 10.
+* The first fuction prints out numbers 0-9 then the last console log will print the value 10.
 This is because i is still valid outside the for loop — the scope of i is not bounded by the curly braces {} 
   */
 
